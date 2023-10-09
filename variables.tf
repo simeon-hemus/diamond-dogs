6f20fa0f7ba04abb19ef3c36fa2152aff3058c69
Simeon Hemus <143768045+simeon-hemus@users.noreply.github.com>
2023-10-10 11:51:39 +1300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,6 +8,11 @@ variable "project" {
   description = "(Required) Application project name."
 }
 
+variable "billable" {
+  type = string
+  description = "(Required) Billable code for project."
+}
+
 variable "environment" {
   type        = string
   description = "(Optional) Application environment for deployment, defaults to development."
